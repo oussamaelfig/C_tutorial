69d22a0181e049bf2ce3428abd691f70e62efae9
oussama EL FIGHA <oelfigha2002gmail.com>
2022-05-13 17:14:10 -0400

    Add input and display array

---
+++ Average.c
@@ -0,0 +1,33 @@
+/*******************************************************************************
+* 
+* Description: Program to find the average of n numbers using arrays
+* It still need some correction
+*
+* Author: Oussama el-figha
+*
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char const *argv[])
+{
+    const int n = 10;
+    int marks[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%s %d %s","Enter the number at the indexe ", i ," : ");
+        scanf("%d", &marks[i]);
+    }
+
+        printf("\n%s\n", "the list of numbers is : ");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d,", marks[i]);
+    }
+    
+    
+    return 0;
+}
